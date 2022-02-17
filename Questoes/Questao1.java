7be780901a8f5ae60606772ed71e42e63b0a7a97
Romenildo Ferreira <53869976+Romenildo@users.noreply.github.com>
2022-02-17 09:12:25 -0300

    adicionado questao 1

--- Questoes/Questao1.java
+++ Questoes/Questao1.java
@@ -1,1 +1,36 @@
 
+/* @Author Romenildo
+ * @Java Version SE-15
+ * 
+ * Questão 01
+ * Algoritmo para mostrar na tela uma escada de tamanho n utilizando
+ *  o caractere(*) e espacos
+ * 
+ * entrada: n = 6
+ * Saída:
+       *
+      **
+     ***
+    ****
+   *****
+  ******
+ */
+package desafioCapgemini;
+
+public class Questao1 {
+
+	public static void main(String[] args) {
+		construirEscada(6);
+
+	}
+	
+	public static void construirEscada(int tamanho) {
+		char [] asteriscos = new char[tamanho];
+		
+		for(int i = tamanho-1; i >=0; i--) {
+			asteriscos[i] = '*';
+			System.out.println(asteriscos);
+			
+		}
+	}
+}
